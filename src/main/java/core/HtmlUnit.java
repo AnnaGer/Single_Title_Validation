7a4bdb0f5bfe717b93cb020e7cf9d84d02976f36
Anna <anny.gerhardt@gmail.com>
2015-02-03 21:00:24 -0600

    Changing HtmlUnit. Using CLA for Url and Title_expected

--- src/main/java/core/HtmlUnit.java
+++ src/main/java/core/HtmlUnit.java
@@ -5,13 +5,22 @@ import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 public class HtmlUnit {
-	public static void main(String[] args) {
+	public static void main(String[] a) {
 
+		if (a.length==0){
+			System.err.print("No arguments");
+			System.exit(2);
+		}
+		
 		WebDriver driver = new HtmlUnitDriver();   // Version 1.3 :: HtmlUnit
 
 		String text_case_id = "TC-002.01";
-		String url = "http://www.learn2test.net";
-		String title_expected = "learn2test.net";
+//		String url = "http://www.learn2test.net";
+//		String title_expected = "learn2test.net";
+		
+		String param[] =a[0].split("\\|");	
+		String url=param[0];
+		String title_expected=param[1];
 		
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
